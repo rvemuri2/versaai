5864b572810ad2d8bb98ce3948be2901bd89de48
rvemuri2 <rohitvemuri@yahoo.com>
2024-10-02 16:58:53 -0700

    Creating authentication with clerk

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { runAi } from "@/actions/ai";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Card, CardHeader, CardContent } from "@/components/ui/card";
+import ReactMarkdown from "react-markdown";
 
 export default function Page() {
   const [response, setResponse] = useState("");
@@ -40,7 +41,11 @@ export default function Page() {
         <Card className="mt-5">
           <CardHeader>AI Response</CardHeader>
           <CardContent>
-            {loading ? <div>Loading...</div> : <div>{response}</div>}
+            {loading ? (
+              <div>Loading...</div>
+            ) : (
+              <ReactMarkdown>{response}</ReactMarkdown>
+            )}
           </CardContent>
         </Card>
       </div>
