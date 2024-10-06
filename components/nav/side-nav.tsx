5218f841390e458b7d79b4973cc28239a96855ca
rvemuri2 <rohitvemuri@yahoo.com>
2024-10-06 15:10:25 -0700

    Adding credit usage for prompts

--- components/nav/side-nav.tsx
+++ components/nav/side-nav.tsx
@@ -6,13 +6,14 @@ import {
   WalletCards,
   Settings,
 } from "lucide-react";
-
-import Link from "next/link";
-
 import { usePathname } from "next/navigation";
+import Link from "next/link";
+import Usage from "@/components/nav/usage";
+import SignUpModal from "@/components/modal/sign-up-modal";
 
 export default function SideNav() {
   const path = usePathname();
+
   const menu = [
     {
       name: "Dashboard",
@@ -35,25 +36,35 @@ export default function SideNav() {
       path: "/dashboard/settings",
     },
   ];
+
+  console.log(path);
+
   return (
-    <div className="h-screen p-5 shadow-sm border">
-      {menu.map((item, index) => (
-        <div
-          key={index}
-          className={`${
-            path === item.path
-              ? "border-primary text-primary"
-              : "hover:border-primary hover:text-primary"
-          } flex m-2 mr-2 p-2 hover:bg-primary cursor-pointer border`}
-        >
-          <div className="flex justify-center items-center md:justify-start w-full">
-            <Link href={item.path} className="flex">
-              <item.icon />{" "}
-              <span className="ml-2 hidden md:inline">{item.name}</span>
-            </Link>
-          </div>
-        </div>
-      ))}
+    <div className="flex flex-col h-full">
+      <ul className="flex-1 space-y-2">
+        {menu.map((item, index) => (
+          <li
+            key={index}
+            className={`${
+              path === item.path
+                ? "border-primary text-primary"
+                : "hover:border-primary hover:text-primary"
+            } flex m-2 mr-2 p-2 rounded-lg cursor-pointer border`}
+          >
+            <div className="flex justify-center items-center md:justify-start w-full">
+              <Link href={item.path} className="flex">
+                <item.icon />{" "}
+                <span className="ml-2 md:inline">{item.name}</span>
+              </Link>
+            </div>
+          </li>
+        ))}
+      </ul>
+
+      <div className="pb-20 mt-auto">
+        <Usage />
+        <SignUpModal />
+      </div>
     </div>
   );
 }
---
+++ components/nav/usage.tsx
@@ -0,0 +1,41 @@
+"use client";
+import React from "react";
+import { useUsage } from "@/context/usage";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function Usage() {
+  const { count, subscribed } = useUsage();
+
+  const credits = Number(process.env.NEXT_PUBLIC_FREE_TIER_USAGE);
+  // const percentage = (count / credits) * 100;
+  const percentage = subscribed ? 100 : Math.min((count / credits) * 100, 100);
+
+  return (
+    <div className="m-2">
+      <div className="rounded-lg shadow border p-2">
+        <h2 className="font-medium">Credits</h2>
+
+        <div className="h-2 bg-slate-500 w-full rounded-full mt-3">
+          <div
+            className="h-2 bg-slate-200 rounded-full"
+            style={{ width: `${percentage}%` }}
+          ></div>
+        </div>
+
+        <h2 className="text-sm my-2">
+          {subscribed
+            ? `Unlimited credits`
+            : `${count} / ${credits} credit used`}
+          {/* {count} / {credits} credit used */}
+        </h2>
+      </div>
+
+      <Link href="/membership">
+        <Button className="w-full my-3" variant="secondary">
+          Upgrade
+        </Button>
+      </Link>
+    </div>
+  );
+}
