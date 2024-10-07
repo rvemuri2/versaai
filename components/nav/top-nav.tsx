24253eb0ae89f7574643d98749d3b2bf4c2e50e9
rvemuri2 <rohitvemuri@yahoo.com>
2024-10-07 16:35:55 -0700

    Updating top nav

--- components/nav/top-nav.tsx
+++ components/nav/top-nav.tsx
@@ -51,11 +51,13 @@ export default function TopNav() {
           <div className="ml-2">
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
-                <Button size="icon">
-                  <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                  <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                  <span className="sr-only">Toggle theme</span>
-                </Button>
+                <span>
+                  <Button size="icon">
+                    <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                    <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                    <span className="sr-only">Toggle theme</span>
+                  </Button>
+                </span>
               </DropdownMenuTrigger>
               <DropdownMenuContent align="end">
                 <DropdownMenuItem onClick={() => setTheme("light")}>
